6826016697e976ff70370a56e0a52d670dfc4914
Arturo Blanco <100625719+arturobdev@users.noreply.github.com>
2026-05-31 11:16:08 -0300

    added userService functions

--- services/userService.js
+++ services/userService.js
@@ -1,7 +1,6 @@
 import { userRepository } from "../repositories/userRepository.js";
 
 class UserService {
-  // TODO: Implement methods to work with user
 
   search(search) {
     const item = userRepository.getOne(search);
@@ -9,6 +8,73 @@ class UserService {
       return null;
     }
     return item;
+  }
+
+  getAll() {
+    return userRepository.getAll();
+  }
+
+  create(userData) {
+    const email = userData.email.toLowerCase();
+
+    const userByEmail = userRepository.getOne({ email });
+
+    if (userByEmail) {
+      throw Error("User with this email already exists");
+    }
+
+    const userByPhone = userRepository.getOne({
+      phone: userData.phone
+    });
+
+    if (userByPhone) {
+      throw Error("User with this phone already exists");
+    }
+
+    return userRepository.create({
+      ...userData,
+      email
+    });
+  }
+
+  update(id, userData) {
+    const user = userRepository.getOne({ id });
+
+    if (!user) {
+      throw Error("User not found");
+    }
+
+    if (userData.email) {
+      const email = userData.email.toLowerCase();
+
+      const existingUser = userRepository.getOne({ email });
+
+      if (existingUser && existingUser.id !== id) {
+        throw Error("User with this email already exists");
+      }
+
+      userData.email = email;
+    }
+
+    if (userData.phone) {
+      const existingUser = userRepository.getOne({
+        phone: userData.phone
+      });
+
+      if (existingUser && existingUser.id !== id) {
+        throw Error("User with this phone already exists");
+      }
+    }
+
+    return userRepository.update(id, userData);
+  }
+
+  delete(id) {
+    const userExisting = userRepository.getOne({ id });
+    if (!userExisting) {
+      throw Error("User not found");
+    }
+    return userRepository.delete(id);
   }
 }
 
